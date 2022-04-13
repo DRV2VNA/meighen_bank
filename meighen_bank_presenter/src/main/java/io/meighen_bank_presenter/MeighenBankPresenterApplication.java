b85ff1a51f621c4b2dbfde3c2dc21d648d0fba64
Dmitry <1664286@gmail.com>
2022-04-13 21:20:15 +0300

    develop | Добавлены основные файлы presenter-а

---
+++ meighen_bank_presenter/src/main/java/io/meighen_bank_presenter/MeighenBankPresenterApplication.java
@@ -0,0 +1,13 @@
+package io.meighen_bank_presenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeighenBankPresenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeighenBankPresenterApplication.class, args);
+	}
+
+}
---
+++ meighen_bank_presenter/src/test/java/io/meighen_bank_presenter/MeighenBankPresenterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.meighen_bank_presenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeighenBankPresenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
