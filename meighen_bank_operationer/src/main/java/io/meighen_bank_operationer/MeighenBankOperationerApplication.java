a75dc6feb160ec1276a5e6b434345d9d3ff410e3
Dmitry <1664286@gmail.com>
2022-04-19 20:10:58 +0300

    MGB-14 | Добавлен микросервис операций

---
+++ meighen_bank_operationer/src/main/java/io/meighen_bank_operationer/MeighenBankOperationerApplication.java
@@ -0,0 +1,13 @@
+package io.meighen_bank_operationer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeighenBankOperationerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeighenBankOperationerApplication.class, args);
+	}
+
+}
---
+++ meighen_bank_operationer/src/test/java/io/meighen_bank_operationer/MeighenBankOperationerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.meighen_bank_operationer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeighenBankOperationerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
